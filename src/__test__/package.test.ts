aaed27bcff566405944cfb8ab70e289df70761e0
HeesuKim0203 <heesukim998@gmail.com>
2024-01-22 17:10:31 +0900

    package test code

---
+++ src/__test__/package.test.ts
@@ -0,0 +1,11 @@
+// import { helloWorld } from "npm-public-package-test"
+
+
+// test('Hello world function test', () => {
+
+//     expect(helloWorld()).toEqual('Hello World from my example modern npm package!')
+    
+// })
+
+// Todo : Add package test
+// Todo : Build 할때 pack.test.ts 무시하도록 설정
